85046fa2f27b8b9fbfa1cbfa434e040216d58c05
Serhii Kirpichev <seriykirpichev@gmail.com>
2022-03-24 11:16:41 +0200

    Added task №1

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,2 +1,13 @@
-//Задача №1
-console.info ("Задача №1");
+//Задача №1 (Написать функцию, возвращает true если символ найден в строке и false если нет.)
+console.info("Задача №1");
+function isSymbolPresentInString(datastring, symbol) {
+ if (typeof datastring === "string" && datastring !== null) {
+    for (let i = 0; i < datastring.length; i++) {
+      if (datastring.charAt(i) === symbol) return true;
+    }
+ }
+  return false;
+}
+const symbol = "c";
+const datastring = "acergf";
+console.log(isSymbolPresentInString(datastring, symbol));
