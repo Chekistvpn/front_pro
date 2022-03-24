5313d94092987c0325221244cfd934b80dcce017
Serhii Kirpichev <seriykirpichev@gmail.com>
2022-03-24 16:44:06 +0200

    Added 2 task

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,13 +1,27 @@
 //Задача №1 (Написать функцию, возвращает true если символ найден в строке и false если нет.)
 console.info("Задача №1");
-function isSymbolPresentInString(datastring, symbol) {
- if (typeof datastring === "string" && datastring !== null) {
-    for (let i = 0; i < datastring.length; i++) {
-      if (datastring.charAt(i) === symbol) return true;
+function isSymbolPresentInString(dataString, symbol) {
+  if (typeof dataString === "string" && dataString !== null) {
+    for (let i = 0; i < dataString.length; i++) {
+      if (dataString.charAt(i) === symbol) return true;
     }
- }
+  }
   return false;
 }
 const symbol = "c";
-const datastring = "acergf";
-console.log(isSymbolPresentInString(datastring, symbol));
+const dataString = "acergf";
+console.log(isSymbolPresentInString(dataString, symbol));
+
+//Задача №2 (Написать функцию, которая возвращает индекс первого найденного символа в строке, или -1 если символ не найден)
+console.info("Задача №2");
+function getSymbolIndex(paramString, anySymbol) {
+  if (typeof paramString === "string" && paramString !== null) {
+    for (let i = 0; i < paramString.length; i++) {
+      if (paramString.charAt(i) === anySymbol) return i;
+    }
+  }
+  return -1;
+}
+const anySymbol = "f";
+const paramString = "acergf";
+console.log("Индекс первого найденного символа", anySymbol, "равен", getSymbolIndex(paramString, anySymbol));
