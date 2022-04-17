354486656af08b4d8aafb088b24012156cd96e5b
Serhii Kirpichev <seriykirpichev@gmail.com>
2022-04-17 16:43:52 +0300

    Added homework  of lesson8

--- lesson8/script.js
+++ lesson8/script.js
@@ -1,9 +1,21 @@
-//Задача №1
-console.info("Задача №1");
-function g
-
-
-
-
-
-
+console.info("Задача №1, функция глубокого копирования объекта");
+function greatCopyObject (sedan, waggon ) {
+  if (typeof sedan === 'object' && typeof waggon === 'object' && typeof sedan !== null && typeof waggon !== null) {
+    if (sedan instanceof Array && waggon instanceof Array) {
+      if (sedan.length !== waggon.length) return false;
+    }
+    for (let key in sedan ) {
+      if (!waggon.hasOwnProperty(key)) return false;
+      if (typeof sedan[key] === 'object' && typeof waggon[key] === 'object' && !(sedan instanceof Array) && !(waggon instanceof Array)) {
+        if (!greatCopyObject (sedan[key], waggon[key])) return false;
+      } else {
+          if (sedan[key] !== waggon[key]) return false
+      }
+    }
+    return true;
+  } else {
+    return sedan === waggon;
+  } 
+}
+let obj = {here: {is: "name", me: [4, 5]}, object: 3};
+console.log(greatCopyObject(obj, {here: {is: "name", me: [4, 5]}, object: 3}));
