7e12226f215fc15aaf6468fc04f58dd0c7dd339b
Serhii Kirpichev <seriykirpichev@gmail.com>
2022-04-17 21:36:26 +0300

    Added homework9 to lesson10

--- lesson9/script.js
+++ lesson9/script.js
@@ -1,2 +1,13 @@
 //Задача №1
-console.info("Задача №1");
+console.info("Задача №1 - Функция при первом вызове запоминает параметр, при втором - суммирует переданный параметр с тем, что передали первый раз и т.д.");
+function calcSum () {
+  let count = 0;
+  return function (number) {
+    return (count += number);
+  };
+}
+let sum = calcSum();
+console.log("Sum(3) -> ф-ция возвращает результат " + sum(3));
+console.log("Sum(5) -> ф-ция возвращает результат " + sum(5));
+console.log("Sum(8) -> ф-ция возвращает результат " + sum(8));
+console.log("Sum(1) -> ф-ция возвращает результат " + sum(1));
