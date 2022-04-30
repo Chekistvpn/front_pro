b3c82d7097042ab5392d223c84fba316a0650e2f
Serhii Kirpichev <seriykirpichev@gmail.com>
2022-04-30 11:25:37 +0300

    Added corrected task

--- lesson8/script.js
+++ lesson8/script.js
@@ -1,21 +1,15 @@
 console.info("Задача №1, функция глубокого копирования объекта");
-function greatCopyObject (sedan, waggon ) {
-  if (typeof sedan === 'object' && typeof waggon === 'object' && typeof sedan !== null && typeof waggon !== null) {
-    if (sedan instanceof Array && waggon instanceof Array) {
-      if (sedan.length !== waggon.length) return false;
+function greatCopyObject(sedan) {
+  let copyObj = Array.isArray(sedan) ? [] : {};
+  for (let key in sedan) {
+    if (sedan.hasOwnProperty(key)) {
+      if ("object" === typeof sedan[key] && sedan[key] !== null)
+        copyObj[key] = greatCopyObject(sedan[key]);
+      else copyObj[key] = sedan[key];
     }
-    for (let key in sedan ) {
-      if (!waggon.hasOwnProperty(key)) return false;
-      if (typeof sedan[key] === 'object' && typeof waggon[key] === 'object' && !(sedan instanceof Array) && !(waggon instanceof Array)) {
-        if (!greatCopyObject (sedan[key], waggon[key])) return false;
-      } else {
-          if (sedan[key] !== waggon[key]) return false
-      }
-    }
-    return true;
-  } else {
-    return sedan === waggon;
-  } 
+  }
+  return copyObj;
 }
-let obj = {here: {is: "name", me: [4, 5]}, object: 3};
-console.log(greatCopyObject(obj, {here: {is: "name", me: [4, 5]}, object: 3}));
+const obj = { here: { is: "name", me: [4, 5] }, object: 1 };
+console.log("Исходный объект", obj);
+console.log("Скопированный объект", greatCopyObject(obj));
