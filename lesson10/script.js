79c909cd9ce23c5c27b0c016678ef448f6f770c4
Serhii Kirpichev <seriykirpichev@gmail.com>
2022-04-30 17:49:03 +0300

    Added generateList function

--- lesson10/script.js
+++ lesson10/script.js
@@ -1,2 +1,25 @@
-//Задача №1
-console.info("Задача №1 ");
+//Задача - Написать функцию generateList. Принимает массив из чисел и массивов чисел, например: [1, 2, [1.1, 1.2, 1.3], 3]. 
+//Нужно сгенерировать список из элементов. Если в массиве встречается массив, то нужно сделать вложенный список.
+let generateParagraf = document.createElement("p");
+generateParagraf.innerText = "Создание нового списка:"; 
+generateParagraf.style.fontWeight = "bold";
+generateParagraf.style.color = "red";
+document.body.appendChild(generateParagraf);
+function generateList(someArray) {
+  let generateUl = document.createElement("ul");
+  generateUl.style.color = "blue";
+  someArray.forEach(function (el) {
+    let generateLi = document.createElement("li");
+    let childElement;
+    if (Array.isArray(el)) {
+      childElement = generateList(el);
+    } else {
+      childElement = document.createTextNode(el);
+    }
+    generateLi.appendChild(childElement);
+    generateUl.appendChild(generateLi);
+  });
+  return generateUl;
+};
+//document.body.appendChild(generateList([1, 2, 3]));
+document.body.appendChild(generateList([1, 2, [1.1, 1.2, 1.3], 3]));
